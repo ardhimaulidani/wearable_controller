6a1d134058467cbcc24e26a90cdb1c94f714082f
ardhimaulidani <ardhi.maulidani@gmail.com>
2024-03-16 21:41:18 +0700

    update latest documentation

--- build/esp-idf/micro_ros_espidf_component/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,6 +0,0 @@
-file(REMOVE_RECURSE
-  "../components/micro_ros_espidf_component/include"
-  "../components/micro_ros_espidf_component/micro_ros_dev"
-  "../components/micro_ros_espidf_component/micro_ros_src"
-  "../components/micro_ros_espidf_component/esp32_toolchain.cmake"
-)
